21dd6041335a3f229fea355e84ebaf99de4f9c77
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-12-03 20:56:45 -0600

    Chapter 3

---
+++ chapter03/contact.py
@@ -0,0 +1,11 @@
+# contact.py
+
+class Contact:
+    
+    all_contacts = []
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self) 
+        
