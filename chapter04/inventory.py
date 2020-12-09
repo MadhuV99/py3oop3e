edc8011dc20d6e01e4b2aabcb40a828a5645be72
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-12-09 12:12:41 -0600

    Chapter 4

---
+++ chapter04/inventory.py
@@ -0,0 +1,54 @@
+# inventory.py
+
+class OutOfStock(Exception):
+    pass
+
+
+class InvalidItemType(Exception):
+    pass
+
+
+class Inventory:
+    def lock(self, item_type):
+        """Select the type of item that is going to
+        be manipulated. This method will lock the
+        item so nobody else can manipulate the
+        inventory until it's returned. This prevents
+        selling the same item to two different
+        customers."""
+        pass
+
+    def unlock(self, item_type):
+        """Release the given type so that other
+        customers can access it."""
+        pass
+
+    def purchase(self, item_type):
+        item_balance = 0
+        """If the item is not locked, raise an
+        exception. If the item_type  does not exist,
+        raise an exception. If the item is currently
+        out of stock, raise an exception. If the item
+        is available, subtract one item and return
+        the number of items left."""
+        # pass
+        raise OutOfStock
+        return item_balance
+
+
+item_type = "widget"
+inv = Inventory()
+inv.lock(item_type)
+try:
+    num_left = inv.purchase(item_type)
+except InvalidItemType:
+    print("Sorry, we don't sell {}".format(item_type))
+except OutOfStock:
+    print("Sorry, {} is out of stock.".format(item_type))
+else:
+    print(
+        "Purchase complete. There are "
+        "{} {}s left".format(num_left, item_type)
+    )
+finally:
+    inv.unlock(item_type)
