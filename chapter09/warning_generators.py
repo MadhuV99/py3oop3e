5485c7f3cb39b177eb216bb364c816076e932b6b
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-12-22 23:30:13 -0600

    Chapter 9

--- chapter09/warning_generators.py
+++ chapter09/warning_generators.py
@@ -3,9 +3,9 @@ import sys
 
 # inname = sys.argv[1]
 # outname = sys.argv[2]
-
 inname = 'sample_log.txt'
 outname = 'no_warnings.txt'
+
 # generator expression
 # with open(inname) as infile:
 #     with open(outname, "w") as outfile:
@@ -16,8 +16,48 @@ outname = 'no_warnings.txt'
 #             outfile.write(l)
 
 # normal loop
+# with open(inname) as infile:
+#     with open(outname, "w") as outfile:
+#         for l in infile:
+#             if "WARNING" in l:
+#                 outfile.write(l.replace("    WARNING", "")) 
+
+# object-oriented
+# class WarningFilter:
+#     def __init__(self, insequence):
+#         self.insequence = insequence
+
+#     def __iter__(self):
+#         return self
+
+#     def __next__(self):
+#         l = self.insequence.readline()
+#         while l and "WARNING" not in l:
+#             l = self.insequence.readline()
+#         if not l:
+#             raise StopIteration
+#         return l.replace("    WARNING", "")
+
+# with open(inname) as infile:
+#     with open(outname, "w") as outfile:
+#         filter = WarningFilter(infile)
+#         for l in filter:
+#             outfile.write(l)
+
+# # Generator with yield
+def warnings_filter(insequence):
+    # for l in insequence:
+    #     if "WARNING" in l:
+    #         yield l.replace("    WARNING", "")
+    yield from (
+                l.replace("    WARNING", "") for l in insequence if "WARNING" in l
+                )
+
+
 with open(inname) as infile:
     with open(outname, "w") as outfile:
-        for l in infile:
-            if "WARNING" in l:
-                outfile.write(l.replace("    WARNING", "")) 
+        filter = warnings_filter(infile)
+        for l in filter:
+            outfile.write(l)
+
+# print(dir(warnings_filter([])))
