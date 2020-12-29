c53f9a2987cd4223aba79d192c1e336f505565ad
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-12-28 22:13:21 -0600

    Chapter 11

--- chapter11/age_calculator.py
+++ chapter11/age_calculator.py
@@ -14,11 +14,41 @@ class AgeCalculator:
             age -= 1
         return age
 
+class DateAgeAdapter:
+    
+    def _str_date(self, date):
+        return date.strftime("%Y-%m-%d")
+    
+    def __init__(self, birthday):
+        birthday = self._str_date(birthday)
+        self.calculator = AgeCalculator(birthday)
+    
+    def get_age(self, date):
+        date = self._str_date(date)
+        return self.calculator.calculate_age(date)
+
+class AgeableDate(datetime.date):
+    
+    def split(self, char):
+        return self.year, self.month, self.day
+
 def main():
     # max = AgeCalculator('1964-03-23')
     # max = AgeCalculator('1969-10-07')
-    max = AgeCalculator('1998-04-16')
-    print('Age:', max.calculate_age('2020-12-28'))
+    # max = AgeCalculator('1998-04-16')
+    # print('Age:', max.calculate_age('2020-12-28'))
+
+    # bday = datetime.date(1998,4,16) 
+    # max = DateAgeAdapter(bday)
+    # today = datetime.date.today()
+    # print('Age:', max.get_age(today))
+
+    bday = AgeableDate(1998,4,16)
+    today = AgeableDate.today()
+    print(today)
+    max = AgeCalculator(bday)
+    print('Age:', max.calculate_age(today))
+
 
 if __name__ == '__main__':
     main() 
